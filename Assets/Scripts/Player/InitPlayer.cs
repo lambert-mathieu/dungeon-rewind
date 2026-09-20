ddf0cbb301c1d3a6c0bdc7811e998582dd2a9280
Mathieu Lambert <mathieulambert48@gmail.com>
2026-09-20 13:30:16 -0400

    Added ilyes

--- Assets/Scripts/Player/InitPlayer.cs
+++ Assets/Scripts/Player/InitPlayer.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class InitPlayer : MonoBehaviour
 {
+    public static PlayerData Data { get; private set; }
+
     [SerializeField] private PlayerData playerData;
     [SerializeField] private GameObject player;
 
     private void OnEnable()
     {
         playerData.Register(player);
+        Data = playerData;
     }
 
     private void OnDisable()
     {
         playerData.Unregister(player);
+
+        if (Data == playerData)
+        {
+            Data = null;
+        }
     }
 }
